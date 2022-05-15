2872333b4a17d5e7a8523c1d861a86a8b17ca101
Stealves <stealves@outlook.com>
2022-05-15 20:10:01 -0300

    Exercicio 1.5.1

---
+++ exercicio-aula-4/pratica-exercicio-151/script.js
@@ -0,0 +1,68 @@
+const studentName = document.getElementById('name');
+const grade = document.querySelectorAll('input[type="number"]');
+const infoInput = document.querySelectorAll('input');
+const addButton = document.querySelector('.button__add');
+
+
+const tableReport = document.getElementById('report--results');
+const saveButton = document.querySelector('.button__localsave');
+
+document.addEventListener('DOMContentLoaded', getReport);
+addButton.addEventListener('click', addReport);
+saveButton.addEventListener('click', saveInLocalStorage);
+
+function getReport() {
+  if (localStorage.getItem('boletim') !== null) {
+    tableReport.innerHTML = localStorage.getItem('boletim');
+  }
+}
+
+function addReport() {
+  // Calculate total and average
+  let gradeTotal = sumTotal(grade);
+  let gradeAverage = Math.floor(sumTotal(grade)/ grade.length);
+
+  // Test result
+  let result;
+  if (gradeAverage > 70) {
+    result = 'Aprovado'
+  } else {
+    result = 'Reprovado'
+  }
+
+  // Create Element
+  let tr = document.createElement('tr');
+  let newReport =`
+    <td>${studentName.value}</td>
+    <td>${gradeTotal}</td>
+    <td>${gradeAverage}</td>
+    <td>${result}</td>
+  `;
+  tr.innerHTML = newReport
+  
+  // Show new report in UI
+  tableReport.appendChild(tr);
+}
+
+// Save in local Storage
+function saveInLocalStorage() {
+  let reports = tableReport.innerHTML;
+
+  if (reports.length > 1) {
+    localStorage.setItem('boletim', reports );
+  } else {
+    alert('Não há dados para salvar!')
+  }
+}
+
+function sumTotal(grades) {
+  let sum = 0;
+
+  for (let i = 0; i < grades.length; i++) {
+    if (parseInt(grades[i].value)) {
+      sum += parseInt(grades[i].value);
+    }
+  }
+
+  return sum
+};
